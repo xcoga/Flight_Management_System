dc2aeb45974cfb169dceaa327290fb4282e24b65
xcoga <103871917+xcoga@users.noreply.github.com>
2025-03-10 12:17:04 +0800

    Removed firebase api key

--- SPA-assessment/src/firebase.js
+++ SPA-assessment/src/firebase.js
@@ -8,7 +8,7 @@ import { getDocs, collection, addDoc, setDoc, doc, getDoc, updateDoc, deleteDoc,
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCQFgNMU9uXDgW7tWtQ0gtS0g3tiljGJm0",
+  apiKey: "", //Removed apiKey for privacy concerns
   authDomain: "flight-management-3f213.firebaseapp.com",
   projectId: "flight-management-3f213",
   storageBucket: "flight-management-3f213.firebasestorage.app",
